d2ec82e3bc74151bb5e7b5660c30609b3cca28de
Daniel Valdez | FullStack Developer <hello@daniel-valdez.com>
2026-06-01 10:38:00 -0400

    feat: create pools types

---
+++ packages/cli/src/pools/types.ts
@@ -0,0 +1,9 @@
+export interface Pool {
+    id: string;
+    name: string;
+    description: string;
+    models: string[];
+    systemPrompt: string;
+    createdAt: string;
+    updateAt: string;
+}
